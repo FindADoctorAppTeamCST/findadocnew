d81b65bf4a49ea7d449f5b699fbb3bf56f169a51
khushwanth <khushwanth.chhetri@gmail.com>
2018-01-05 22:13:06 +0600

    Commit before Main Feature Branch

--- app/src/main/java/bt/gov/jdwnrh/www/findadoctor/CatTarget.java
+++ app/src/main/java/bt/gov/jdwnrh/www/findadoctor/CatTarget.java
@@ -3,11 +3,11 @@ package bt.gov.jdwnrh.www.findadoctor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchAct extends AppCompatActivity {
+public class CatTarget extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_cat_target);
     }
 }
--- app/src/main/java/bt/gov/jdwnrh/www/findadoctor/Links.java
+++ app/src/main/java/bt/gov/jdwnrh/www/findadoctor/Links.java
@@ -6,4 +6,5 @@ package bt.gov.jdwnrh.www.findadoctor;
 
 public class Links {
     public final String playstoreLink="https://play.google.com/store/apps/details?id=com.g2c.jdw&hl=en";
+    public final String serverIPAddress=""; //The IP Address for the database Server (Lcalhost during the development)...
 }
