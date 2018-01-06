b14b344fa6098d1fb70b4e5fdf3d789e61a8d357
khushwanth <khushwanth.chhetri@gmail.com>
2018-01-06 11:51:05 +0600

    Final Icon Clicking Done...

--- app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DocPage.java
+++ app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DocPage.java
@@ -3,7 +3,7 @@ package bt.gov.jdwnrh.www.findadoctor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.graphics.Color;
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,37 +18,28 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CatTarget extends AppCompatActivity {
-    TextView titleStr,nm_t,st_t;
-    String passTitle;
+public class DocPage extends AppCompatActivity {
+    String doc_id;
+    TextView doc_name_target;
     private RequestQueue requestQueue;
-    private static final String URL = "http://172.25.33.57/fadapp/mirror.php";
+    private static final String URL = "http://172.23.23.86/fadapp/profile.php";
     private StringRequest request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cat_target);
-        titleStr=(TextView)findViewById(R.id.speTitle);
-        passTitle=getIntent().getExtras().getString("Value");
-        titleStr.setText(passTitle);
-
-        nm_t=(TextView)findViewById(R.id.nm);
-        st_t=(TextView)findViewById(R.id.st);
+        setContentView(R.layout.activity_doc_page);
+        doc_id=getIntent().getExtras().getString("doc_emp_id");
         requestQueue = Volley.newRequestQueue(this);
+        doc_name_target=(TextView)findViewById(R.id.doc_name);
+
         request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
                     JSONObject jsonobject = new JSONObject(response);
                     if (jsonobject.names().get(0).equals("name")) {
-                        nm_t.setText(jsonobject.getString("name"));
-                        if(jsonobject.getString("stat").equals("1")) {
-                            st_t.setText("IN");
-                            st_t.setTextColor(Color.parseColor("#00FF00"));
-                        } else {
-                            st_t.setText("OUT");
-                            st_t.setTextColor(Color.parseColor("#00FF00"));
-                        }
+                        doc_name_target.setText(jsonobject.getString("name"));
+                        //Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -63,7 +54,7 @@ public class CatTarget extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 HashMap<String, String> hashMap = new HashMap<String, String>();
-                hashMap.put("speid", passTitle);
+                hashMap.put("docid", doc_id);
                 return hashMap;
             }
         };
