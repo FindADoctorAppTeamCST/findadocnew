93d5079c6ed25bff1dd6090b0e2b166ad49f2a61
khushwanth <khushwanth.chhetri@gmail.com>
2018-03-25 23:14:27 +0600

    docLoginFirstCompile

--- app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DoctorLogin.java
+++ app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DoctorLogin.java
@@ -3,9 +3,13 @@ package bt.gov.jdwnrh.www.findadoctor;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -28,9 +32,23 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A login screen that offers login via username/password.
@@ -95,21 +113,14 @@ public class DoctorLogin extends AppCompatActivity {
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
         // Check for a valid email address.
         if (TextUtils.isEmpty(username)) {
             mUsernameView.setError(getString(R.string.error_field_required));
             focusView = mUsernameView;
             cancel = true;
-        } else if (!isEmailValid(username)) {
-            mUsernameView.setError(getString(R.string.error_invalid_email));
-            focusView = mUsernameView;
+        } else if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError("This field is required");
+            focusView = mPasswordView;
             cancel = true;
         }
 
@@ -122,16 +133,6 @@ public class DoctorLogin extends AppCompatActivity {
             // perform the user login attempt.
             showProgress(true);
         }
-    }
-
-    private boolean isEmailValid(String uname) {
-        //TODO: Replace this with your own logic
-        return true;
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 6;
     }
 
     /**
@@ -178,6 +179,10 @@ public class DoctorLogin extends AppCompatActivity {
 
         private final String mEmail;
         private final String mPassword;
+        Context context;
+        String url = "http://findadoc.dx.am/findadocandroid/login.php";
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
         UserLoginTask(String email, String password) {
             mEmail = email;
@@ -189,9 +194,53 @@ public class DoctorLogin extends AppCompatActivity {
             // TODO: attempt authentication against a network service.
 
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                RequestQueue queue = Volley.newRequestQueue(DoctorLogin.this);
+                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResp = new JSONObject(response);
+                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                            builder.setTitle("Login Status: ");
+                            AlertDialog alertdialog;
+                            if (jsonResp.getString("resp").equals("1")) {
+                                startActivity(new Intent(getApplication(), DocProfile.class));
+                            } else {
+                                builder.setMessage(" Error: Username or Password is Incorrect");
+                                builder.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(context, "Enter Correct Credentials and Log In again", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                                alertdialog = builder.create();
+                                alertdialog.show();
+                            }
+
+                        } catch (JSONException jsonError) {
+                            Toast.makeText(DoctorLogin.this, "Error Connecting to Server. Try again.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(DoctorLogin.this, "No Internet Connection\nYou need an Active Internet Connection to send Feedback", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("user",username);
+                        map.put("pass", password);
+
+                        return map;
+                    }
+                };
+                queue.add(request);
+
+            } catch (Exception e) {
                 return false;
             }
             return true;
