b3d72d20ac3439b5f7ce4ea4948ac69345dc5b26
Deetachi <deepikasbr@gmail.com>
2018-05-02 22:13:00 +0600

    logout

--- app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DocProfile.java
+++ app/src/main/java/bt/gov/jdwnrh/www/findadoctor/DocProfile.java
@@ -6,6 +6,8 @@ import android.media.Image;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -68,6 +70,31 @@ public class DocProfile extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.doclogout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logout) {
+            startActivity(new Intent(getApplication(),MainActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
     public void onSwitchClick(View v) {
         if(aSwitch.isChecked()) {
             Toast.makeText(this, "Doctor is IN", Toast.LENGTH_SHORT).show();
